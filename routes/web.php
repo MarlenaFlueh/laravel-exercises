7f0882de1427ed2bec2aabcc880b899a1f5838dd
MarlenaFlueh <mf@esyoil.com>
2018-08-07 08:59:21 +0200

    add new route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@
  */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = [
+        'go to store',
+        'eat lunch',
+        'walk the dog'
+    ];
+
+    return view('welcome', compact('tasks'));
 });
 
 Route::get('/about', function () {
