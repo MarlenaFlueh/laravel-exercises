611179a74d387869001049f98373677df58faf23
MarlenaFlueh <mf@esyoil.com>
2018-08-07 08:59:39 +0200

    add array, show it

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,10 @@
     <title>blog</title>
 </head>
 <body>
-    <h1>hello world</h1>
+    <ul>
+    @foreach ($tasks as $task)
+        <li>{{$task}}</li>
+    @endforeach
+    </ul>
 </body>
 </html>
